aac91216ccfa038bb5432f8a45bad996a5f5fc03
Matt Uttridge <uttrasey@gmail.com>
2018-01-17 14:48:58 +0000

    Added link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => <div>Hello world! <Link to="a/">Problem demonstration</Link></div>
